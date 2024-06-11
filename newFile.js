26e01cae336710d0188eb8b09dff37e2e67e0400
sisiemrah <73857817+sisirmerah@users.noreply.github.com>
2024-06-11 18:52:21 +0700

    portfolio tailwind css

---
+++ newFile.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container,
+    center: true,
+    padding: "16px",
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
